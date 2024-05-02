a38e8e7907568db27f6b1c4875a508c0ad5857cf
VikiMaiselman <maiselmanwikki@gmail.com>
2024-05-02 08:12:50 +0300

    remove unnecessary import

--- src/components/WordInputs.jsx
+++ src/components/WordInputs.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Cell from "./Cell";
 
 import wordArray from "../data/dictionary.js";
